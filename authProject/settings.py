1c6413b891a695f7a48ca7dd689924ff670efd5d
arun3q <arun3q@gmail.com>
2018-09-23 14:15:21 +0000

    changed DB

--- authProject/settings.py
+++ authProject/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'authProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# MySQL DB server
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/ubuntu/workspace/mysql_db_set.cnf',
+        },
     }
 }
 
